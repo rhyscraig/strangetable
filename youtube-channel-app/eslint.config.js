fdc1ebf6ac9580cf3bef1df23f3691384dedee22
Craig Hoad <craighoad@Craigs-MacBook-Pro.local>
2025-08-20 21:53:46 +0100

    init commit to build

--- youtube-channel-app/eslint.config.js
+++ youtube-channel-app/eslint.config.js
@@ -1,13 +1,15 @@
 // eslint.config.js
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import jsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js"; // <-- Import jsx-runtime separately
 import prettierConfig from "eslint-config-prettier";
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ...pluginReactConfig, // Apply the recommended rules
+    ...jsxRuntime, // Apply the JSX runtime rules
     settings: {
       react: {
         version: "detect", // Automatically detect the React version
@@ -21,11 +23,7 @@ export default [
         },
       },
     },
-    // This part tells ESLint to use the new JSX runtime, fixing the error
-    ...pluginReactConfig.configs.recommended['jsx-runtime'].files,
   },
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
   prettierConfig,
 ];
